22badd35a7af37a0a1505564cd8d1a3628c6cf7d
Rick Schultz <rick-schultz@hotmail.com>
2021-08-23 17:14:12 -0300

    react helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Helmet } from 'react-helmet';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <title>Colinas do Sul - iTUDE</title>
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
