7b28cc5019608e7c3912997ae8399544527ba3d2
Rick Schultz <rick-schultz@hotmail.com>
2021-08-25 17:35:05 -0300

    aos fix

--- src/components/mainBig/MainBig.jsx
+++ src/components/mainBig/MainBig.jsx
@@ -13,7 +13,7 @@ export default function MainBig() {
   Aos.init({})
   }, []);
   return (
-<div className="mainBig" data-aos="zoom-in"data-aos-duration="1200"data-aos-delay="200" data-aos-once="true" >
+<div className="mainBig" data-aos="zoom-in"data-aos-duration="1500"data-aos-delay="300" data-aos-once="true" >
 <CasalBig/>
 
   <div id="squareBig">
