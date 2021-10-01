3436a8e67e034dcfa0ac9ae23b40ecb3b696624f
Rick Schultz <rick-schultz@hotmail.com>
2021-10-01 16:46:53 -0300

    mudar link

--- src/components/casalBig/CasalBig.jsx
+++ src/components/casalBig/CasalBig.jsx
@@ -5,7 +5,7 @@ export default function CasalBig() {
  return (
        <div id="casalBig" className="my-4">
         <img src={Familia} alt="foto casal"/>
-          <button><a href="https://api.whatsapp.com/send?phone=5535998690427" target="_blank" rel="noreferrer">MAIS INFORMAÇÕES!</a></button>
+          <button><a href="https://online.crm.anapro.com.br/WebCRMService/Pages/chat/cliente/v2/ChatClienteFaleConosco.aspx?conta=b5PfcEw_G6E1&keyIntegradora=2D031627-1E73-48BE-9729-B9B0BA708F30&keyAgencia=2D031627-1E73-48BE-9729-B9B0BA708F30&strDir=atitude&campanha=3iqqh5Q5dEw1&canal=b5H4krENDc41&produto=VlX13i2EiQk1&strmidia=Landing+page&strpeca=&usuarioEmail=&strgrupopeca=&strcampanhapeca=&nome=&email=&telefoneDDD=&telefone=&strTexto=&keyexterno=&urlep=&urlcp=&urlca=&urlat=&strMostrarTopo=false&strAutoSubmit=true&strU" target="_blank" rel="noreferrer">MAIS INFORMAÇÕES!</a></button>
        </div>
  )
 }
